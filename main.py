64459b26b8eb025567898627d8fda1e380af5bbe
karan bhardwaj <karan.bhardwaj1108@gmail.com>
2017-06-28 16:35:28 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,9 +1,127 @@
-from spy_details import spy,default_spy
-from datetime import datetime
+# Spy chat program to send secret message b/w spies
+# importing Chat class
+# importing Spy class and its associated methods
+# stand alone functions will come in handy also
+from spy_details import Spy
 
 
-print("Hello !! Welcome to spychat")
+# a function that updates the status using Spy methods
+# it checks if there is a status update already
+#   if that,s the case then it shows the the current status and user has two options
+#   Either choose status from previous status updates or add a new one altogether
+# if there is no status update then it asks to add a new status altogether
+# in either of the cases at last it shows the current status
+def status_update():
+    if spy.status is None:
+        print("You do not have any status ")
+        input_status = input("Enter Status to update")
+        spy.update_spy_status(input_status)
+        print("Your status : ")
+        spy.show_status()
+    else:
+        print("Your current Status is : ")
+        spy.show_status()
+        user_choice = input("Enter y if you want to update status from previous status updates : ")
+        if user_choice == "Y" or user_choice == "y":
+            print("These are your previous status updates :")
+            spy.show_previous_status()
+            user_choice1 = int(input("Enter the number of the status update: "))
+            spy.update_spy_status(spy.pool_of_status[user_choice1 - 1])
+            print("Your status : \n")
+            spy.show_status()
+        else:
+            input_status = input("Enter Status to update")
+            spy.update_spy_status(input_status)
+            print("Your status : ")
+            spy.show_status()
+    show_menu()
 
-question="Do you want to continue as "+default_spy.salutation+" "+default_spy.name+" (Y/N) ??"
 
-answer=input(question)
+# This function asks user to enter friend details and if they fulfill the criteria for a spy
+# then it adds the friend to the friends list of our spy
+# if a friend is added it notifies the user by printing the message "friend added"
+def add_a_friend():
+    pass
+
+# This function uses Steganography to encode the message and send it to the desired friend
+# it calls the Spy method named select_a_friend to select a friend out of online friends
+def send_secret_msg():
+    pass
+
+
+def read_secret_msg():
+    pass
+
+
+def read_chats():
+    pass
+
+
+# This function shows menu to the user
+# there are 6 choices to choose from
+# each choice calls a function to handle that choice
+# valid choices result in call to the corresponding function
+# 6th and any other choice halts the program
+def show_menu():
+    menu_choice = int(input("What do you want to do? \n 1. Add a status update \n 2. Add a friend \n 3. Send a secret "
+                            "message \n 4. Read a secret message \n 5. Read Chats from a user \n 6. Close Application "
+                            "\n "))
+    if menu_choice == 1:
+        status_update()
+    elif menu_choice == 2:
+        num_of_friends = add_a_friend()
+        print("You have {} friends".format(num_of_friends))
+        show_menu()
+    elif menu_choice == 3:
+        send_secret_msg()
+    elif menu_choice == 4:
+        read_secret_msg()
+    elif menu_choice == 5:
+        read_chats()
+    else:
+        exit()
+
+
+# A function that asks the user to entered the details if user isn't the default user
+# it creates a spy object that overrides the default one
+def make_a_spy():
+    pass
+
+
+# Starting the application with a welcome note
+print("Welcome to Spy chat Application ")
+
+# creating a default spy with parameters as under
+spy = Spy("Bond", "Mr.", 30, 4.8)
+
+# Assigning our default spy some number of statuses to choose from pool of statuses
+spy.pool_of_status = ["I'm Bond", "At your service", "bow down to the king"]
+
+# Assigning our spy some friends
+# we create to Spy objects and assign them name "friend1" & "friend2"
+# we then add these friends to spy's friend list
+friend1 = Spy("Paras", "Mr", 21, 4.99)
+friend2 = Spy("Jagdish", "Mr", 21, 2.99)
+spy.add_spy_friend(friend1)
+spy.add_spy_friend(friend2)
+
+# Asking user to continue as the spy we made above
+choice = input("Do you want to continue as {} {} (Y or N) ".format(spy.get_salutation(), spy.get_name()))
+
+# Checking if user is a default one or not
+if choice == "Y" or choice == "y":
+    ask_for_info = False
+else:
+    ask_for_info = True
+
+# if user was not the default one we let the "make_a_spy" method handle the task of collecting spy info
+# otherwise welcome him as default spy
+if ask_for_info:
+    spy = make_a_spy()
+
+# Welcome the spy (We use the spy object and its methods)
+print("Welcome {} {} age : {}, rating : {:0.2f}"
+      .format(spy.get_salutation(), spy.get_name(), spy.get_age(), spy.get_rating()))
+
+# We call the "show_menu" function to let the user make a choice from the menu
+show_menu()
--- spy_details.py
+++ spy_details.py
@@ -1,12 +1,60 @@
 # This file contains the default spy data which would be imported in the main.py
 
 #This class defines the specifications of a spy
-class spy:
-    def __init__(self, name, salutation, age, rating):
-        self.name=name
-        self.salutation=salutation
-        self.age=age
-        self.rating=rating
+class Spy:
+    # below are the spy methods
+    # constructor for initialization of a spy
 
-#Declaring the default spy
-default_spy=spy('Bond','Mr',32,4.8)
+    def __init__(self, spyname, salutation, age, rating):
+        self.spyname = spyname
+        self.salutation = salutation
+        self.age = int(age)
+        self.rating = float(rating)
+        self.spy_is_online = True
+        self.status = None
+        self.pool_of_status = []
+        self.friend_list = []
+        self.chats = []
+
+    # accessor method to get spy name
+
+    def get_name(self):
+        return self.spyname
+
+    # accessor method to get spy salutation
+
+    def get_salutation(self):
+        return self.salutation
+
+    # accessor method for spy age
+
+    def get_age(self):
+        return self.age
+
+    # accessor method for spy rating
+
+    def get_rating(self):
+        return self.rating
+
+    # it takes a string as argument and sets the string as spy's status
+    # it appends the status to the pool of status
+    def update_spy_status(self, input_status):
+        self.status = input_status
+        self.pool_of_status.append(input_status)
+
+    # When called it prints the status on screen
+    def show_status(self):
+        print(self.status)
+
+    # When called shows the previous statuses if any
+    def show_previous_status(self):
+        j = 1
+        for i in self.pool_of_status:
+            print("{}. {}".format(j, i))
+            j = j + 1
+
+    # Takes argument a spy object which is friend of spy
+    # and appends it to the spy's function
+    def add_spy_friend(self, friend):
+        self.friend_list.append(friend)
+
